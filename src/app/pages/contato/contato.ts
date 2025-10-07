ad000696b8dc4f11d5265e404993e0c3f993cd5b
CAIO CÉSAR SANTOS PINTO <025.027492@universidadesenaicimatec.edu.br>
2025-10-07 15:32:49 -0300

    Página de Contato Reworked

--- src/app/pages/contato/contato.ts
+++ src/app/pages/contato/contato.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contato',
-  imports: [],
   templateUrl: './contato.html',
-  styleUrl: './contato.css'
+  styleUrls: ['./contato.css']
 })
 export class ContatoComponent {
+  
+  public isExpanded: boolean[] = [false, false, false];
 
+  toggleAnswer(index: number): void {
+    this.isExpanded[index] = !this.isExpanded[index];
+  }
 }
-
